f3ae23f5d357a55dfd4c42fd5e72d0d1c99200eb
hitokun-s <jgpuauno@gmail.com>
2021-03-10 10:11:46 +0900

    WIP send post_approved email

--- plugin.rb
+++ plugin.rb
@@ -18,4 +18,27 @@ load File.expand_path('lib/wada-test/engine.rb', __dir__)
 
 after_initialize do
   # https://github.com/discourse/discourse/blob/master/lib/plugin/instance.rb
+
+  module PrependMethods2
+    def post_approved(user, opts)
+      notification_email(user, opts)
+    end
+  end
+  UserNotifications.prepend PrependMethods2
+
+  class NotificationEmailer::EmailUser
+    def post_approved
+      enqueue :post_approved
+    end
+  end
+
+  class Jobs::UserEmail
+    module PrependMethods
+      def always_email_regular?(user, type)
+        return true
+        # type.to_s != "user_private_message" && user.user_option.email_level == UserOption.email_level_types[:always]
+      end
+    end
+    prepend PrependMethods
+  end
 end
